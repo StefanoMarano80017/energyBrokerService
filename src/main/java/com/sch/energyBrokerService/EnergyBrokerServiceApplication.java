6ff1fffb3f3e4e503b1624e9651c691fafc0f4a9
Stefano <stefanomarano2010@gmail.com>
2026-01-09 17:25:56 +0100

    removing redis support

--- src/main/java/com/sch/energyBrokerService/EnergyBrokerServiceApplication.java
+++ src/main/java/com/sch/energyBrokerService/EnergyBrokerServiceApplication.java
@@ -2,8 +2,10 @@ package com.sch.energyBrokerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EnergyBrokerServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sch/energyBrokerService/application/service/tariff/query/TariffQueryHandler.java
+++ src/main/java/com/sch/energyBrokerService/application/service/tariff/query/TariffQueryHandler.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
