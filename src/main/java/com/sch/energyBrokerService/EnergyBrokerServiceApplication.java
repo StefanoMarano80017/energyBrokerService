0d4f9b5aad8fd248c54ebf01586ae54d108f21cf
Stefano <stefanomarano2010@gmail.com>
2026-01-09 17:47:41 +0100

    fix pom for logging

--- src/main/java/com/sch/energyBrokerService/EnergyBrokerServiceApplication.java
+++ src/main/java/com/sch/energyBrokerService/EnergyBrokerServiceApplication.java
@@ -2,10 +2,8 @@ package com.sch.energyBrokerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class EnergyBrokerServiceApplication {
 
 	public static void main(String[] args) {
